9f4390cb5c342f097177de4e53e186c06939f841
Nicholas Tay <nicholas_tay@mymail.sutd.edu.sg>
2019-03-23 01:16:28 +0800

    Unity Firebase Database test working example code in playerscores.cs

---
+++ Source/multiplayer test/Assets/PlayerScores.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Proyecto26;
+
+public class PlayerScores : MonoBehaviour
+{
+    public Text scoreText;
+    public InputField nameText;
+    private System.Random random = new System.Random();
+
+    public static int player_score;
+    public static string player_name;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player_score = random.Next(0,100);
+        scoreText.text = "Score:" + player_score.ToString();
+    }
+
+    public void OnSubmit()
+    {
+        player_name = nameText.text;
+        PostToDatabase();
+    }
+
+    public void PostToDatabase()
+    {
+        User user = new User();
+        RestClient.Post("https://project-finch-database.firebaseio.com/.json", user);
+    }
+
+}
---
+++ Source/multiplayer test/Assets/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+
+public class User
+{
+    public String userName;
+    public int userScore;
+
+    public User()
+    {
+        userName = PlayerScores.player_name;
+        userScore = PlayerScores.player_score;
+    }
+}
