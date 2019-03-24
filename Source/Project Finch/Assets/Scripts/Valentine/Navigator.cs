57b60d925c7f8a213f8560bd92b363606a4bb563
GENERAL-VALENTI\user <zwewint_naing@hotmail.com>
2019-03-24 21:03:05 +0800

    Added 2 scenes - Stats and Store.
    Clicking on a button on Stats will go to Store, and from the Store, you can go back to the stats. This is done with separate scenes and with Navigator C# script (renamed from MainMenu). This will be the primary file to select the scenes.
    
    For the character stats screen, it is still in its preliminary state, and clicking from Soldier #1 to Soldier #5 icons will only take you to the one available Stats screen.

---
+++ Source/Project Finch/Assets/Scripts/Valentine/Navigator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Navigator : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Game Quit!");
+        Application.Quit();
+        SceneManager.LoadScene(1); // Might need to remove this after it's done.
+    }
+
+    public void GoToStore()
+    {
+        Debug.Log("Going from Stats to Store...");
+        SceneManager.LoadScene(4);
+    }
+
+    public void BacktoStats()
+    {
+        Debug.Log("Going back to Stats...");
+        SceneManager.LoadScene(3);
+    }
+
+}
