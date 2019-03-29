9ba629cda9e5a66491bcb469b9e56382cbd83090
Nicholas Tay <nicholas_tay@mymail.sutd.edu.sg>
2019-03-29 12:07:52 +0800

    Added get from database

--- Source/multiplayer test/Assets/PlayerScores.cs
+++ Source/multiplayer test/Assets/PlayerScores.cs
@@ -10,6 +10,7 @@ public class PlayerScores : MonoBehaviour
     public InputField nameText;
     private System.Random random = new System.Random();
 
+    User user = new User();
     public static int player_score;
     public static string player_name;
 
@@ -26,14 +27,33 @@ public class PlayerScores : MonoBehaviour
     public void OnSubmit()
     {
         player_name = nameText.text;
+        scoreText.text = "Score:" + player_score.ToString();
         PostToDatabase();
+    }
+
+    public void OnGetScore()
+    {
+        RetrieveFromDatabase();
+    }
+
+    private void UpdateScore()
+    {
+        scoreText.text = "Score:"+ user.userScore;
     }
 
     //called to post to firebase.
     public void PostToDatabase()
     {
         User user = new User(); //user is a class that contains player_name and player_score, which will be posted to firebase
-        RestClient.Post("https://project-finch-database.firebaseio.com/.json", user); //link is the firebase i created
+        RestClient.Put("https://project-finch-database.firebaseio.com/"+player_name+".json", user); //link is the firebase i created
     }
 
+    public void RetrieveFromDatabase()
+    {
+        RestClient.Get<User>("https://project-finch-database.firebaseio.com/"+nameText.text+".json").Then(response =>
+        {
+            user = response;
+            UpdateScore();
+        });
+    }
 }
