cfb510ee6b41d85b3b1e70c9af50774246118b61
GENERAL-VALENTI\user <zwewint_naing@hotmail.com>
2019-04-11 04:43:33 +0800

    Better Main Menu with working controls. The ability to change SFX and Music volumes are working, as well as with working Quality settings.
    - For Quality, you can choose from Low, Medium, or High.
    
    - MainMixer is needed for the volume control.

---
+++ Source/Project Finch/Assets/Scripts/Valentine/OptionsScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class OptionsScript : MonoBehaviour {
+
+    public AudioMixer audioMixer;
+
+    public void SetSFXVolume (float volume)
+    {
+        audioMixer.SetFloat("SFXVolume", volume);
+    }
+
+    public void SetMusicVolume(float volume)
+    {
+        audioMixer.SetFloat("MusicVolume", volume);
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+}
