bc456f5d003770163055c80a191cb9119f5310fa
GENERAL-VALENTI\user <zwewint_naing@hotmail.com>
2019-04-16 19:01:16 +0800

    Soldier List is the one that you get initially. It will pull the list/name of 5 soldiers that are currently in your squad/use.
    Clicking on one of the names, by default, should try to match that soldier's ID/name with the database and in turn, import a bunch of data that are relevant to the soldier.
    For this instance, clicking on "Catherine", by design, *should* load relevant data about her. But for now, I simply used a local array to test out the text/dialogue boxes.

---
+++ Source/Project Finch/Assets/Scripts/Valentine/SoldierData.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoldierData :MonoBehaviour
+
+{
+    public Text title;
+    public Text ID;
+    public Text Level;
+    public Text Experience;
+    public Text Bio;
+    public Text HP;
+    public Text Fatigue;
+    public Text Mobility;
+    public Text Aim;
+
+    public string[] imported;
+    // This time, it's for that particular soldier.
+    // I am to assume that these strings of data are in array form.
+
+    
+    // void onClick(int soldierID)
+    // The code below should only work when the name is clicked on the previous screen.
+    // For testing purposes, Awake() is used.
+
+    void Awake()
+    {
+        // imported = Resources.LoadAll<Text>("...");
+        // To implement an import from the server. For now, use a local array.
+        // On click, match with the Soldier's unique ID, and pull their array of results.
+
+        imported = new string[] { "Catherine", "10005", "1","500","Your typical soldier. Will get job done.", "200","10","5","3" };
+
+        title.text = imported[0];
+        ID.text = imported[1];
+        Level.text = imported[2];
+        Experience.text = imported[3];
+        Bio.text = imported[4];
+        HP.text = imported[5];
+        Fatigue.text = imported[6];
+        Mobility.text = imported[7];
+        Aim.text = imported[8];
+
+    }
+
+
+
+}
---
+++ Source/Project Finch/Assets/Scripts/Valentine/SoldierList.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoldierList : MonoBehaviour
+{
+    public string[] imported;
+    
+    public Text SoldierName1 = null;
+    public Text SoldierName2 = null;
+    public Text SoldierName3 = null;
+    public Text SoldierName4 = null;
+    public Text SoldierName5 = null;
+
+    void Awake()
+    {
+        // imported = Resources.LoadAll<Text>("...");
+        // To implement an import from the server. For now, use a local array.
+        // Possible to get an array of soldier IDs, then extract their names?
+
+        imported = new string[] { "Catherine", "Markus", "Ryan", "Clarissa", "Janet" };
+
+        SoldierName1.text = imported[0];
+        SoldierName2.text = imported[1];
+        SoldierName3.text = imported[2];
+        SoldierName4.text = imported[3];
+        SoldierName5.text = imported[4];
+
+    }
+
+
+
+
+}
