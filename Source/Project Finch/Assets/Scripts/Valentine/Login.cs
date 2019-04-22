5c29ec27d191c09824828e4dd579de3b11f3874b
Nicholas Tay <nicholas_tay@mymail.sutd.edu.sg>
2019-04-22 10:37:09 +0800

    login -> main menu screen working

--- Source/Project Finch/Assets/Scripts/Valentine/Login.cs
+++ Source/Project Finch/Assets/Scripts/Valentine/Login.cs
@@ -15,19 +15,10 @@ public class Login : MonoBehaviour
         Debug.Log("signing in..");
         string user_name = userName.text;
         string pass_word = password.text;
-        StartCoroutine(login(user_name, pass_word));
+        Runner_call.Coroutines.Add(login(user_name, pass_word));
 
 
     }
-
-    //public IEnumerator loadingBar (string sceneIndex)
-    //{
-    //    while (!MenuView.PlayerAccount.currentPlayer.dataLoaded)
-    //    {
-    //        for (int i)
-    //    }
-    //    yield return null;
-    //}
 
     public IEnumerator login(string user, string password)
     {
@@ -44,11 +35,9 @@ public class Login : MonoBehaviour
             {
                 
                 waitingForLogin = false;
-                gameObject.SetActive(true);
             }
             yield return new WaitForSeconds(0.25f);
         }
-        Debug.Log("asdasdasdnumOfSoldiers: " + MenuView.PlayerAccount.currentPlayer.numberOfSoldiers);
         SceneManager.LoadSceneAsync("Main Menu");
         yield return null;
     }
