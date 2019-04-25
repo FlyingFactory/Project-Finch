553ac9c6151c49471e13dd714b438d18eb9b5af7
FlyingFactory <36451516+FlyingFactory@users.noreply.github.com>
2019-04-25 21:44:29 +0800

    Tab between login fields and enter to submit

---
+++ Source/Project Finch/Assets/Scripts/Merrick/MenuView/InputFieldTabGroup.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MenuView {
+
+    public class InputFieldTabGroup : MonoBehaviour {
+
+        [SerializeField] private List<UnityEngine.UI.InputField> Fields = new List<UnityEngine.UI.InputField>();
+        [SerializeField] private UnityEngine.UI.Button EnterButton = null;
+
+        private void Start() {
+            if (Fields.Count == 0) Destroy(this);
+        }
+
+        private void Update() {
+            if (Input.GetKeyDown(KeyCode.Tab)) {
+                for (int i = 0; i < Fields.Count; i++) {
+                    if (Fields[i].isFocused) {
+                        Fields[i].DeactivateInputField();
+                        Fields[(i + 1) % Fields.Count].ActivateInputField();
+                        break;
+                    }
+                }
+            }
+            if (EnterButton != null && (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter))) {
+
+                EnterButton.onClick.Invoke();
+            }
+        }
+    }
+}
